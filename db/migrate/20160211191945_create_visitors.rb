7e4e9e3c05c1217309debb9338aadffdcac60342
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-11 19:27:32 +0000

    create Visitors table

--- db/migrate/20160211191945_create_visitors.rb
+++ db/migrate/20160211191945_create_visitors.rb
@@ -1,7 +1,8 @@
 class CreateVisitors < ActiveRecord::Migration
   def change
     create_table :visitors do |t|
-
+      t.string :name
+      t.string :email
       t.timestamps null: false
     end
   end
