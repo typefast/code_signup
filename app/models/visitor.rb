edecda49e6f98bdb6aa6342be6227ba4ff105e02
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-11 20:09:48 +0000

    Add name field to form

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,5 +1,6 @@
 class Visitor < ActiveRecord::Base
   
-  validates_presence_of :email
+  validates_presence_of :email, length: {minimum: 5}
+  validates_presence_of :name, length: {minimum: 2}
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 end
