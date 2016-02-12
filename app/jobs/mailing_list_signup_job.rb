05cf76ac0c4f279ef8289704d7fe29ae809c40e2
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-12 20:39:01 +0000

    Add the subscribe method to the MailingList Job

--- app/jobs/mailing_list_signup_job.rb
+++ app/jobs/mailing_list_signup_job.rb
@@ -5,4 +5,16 @@ class MailingListSignupJob < ActiveJob::Base
     user.subscribe
   end
   
+  def subscribe(user)
+    mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
+    list_id = Rails.application.secrets.mailchimp_list_id
+    result = mailchimp.lists(list_id).members.create(
+      body: {
+        email_address: user.email,
+        status: 'subscribed',
+        merge_fields: {FNAME: user.name}
+      })
+      Rails.logger.info("Subscribed #{user.email} to mailchimp") if result
+  end
+  
 end
