8d796ce5cba944eb9730bc5cd0a0746cdfec7c42
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-16 09:46:18 +0000

    change call back to after commit and add error handling to mailchimp

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -3,7 +3,7 @@ class Visitor < ActiveRecord::Base
   validates_presence_of :email, length: {minimum: 5}
   validates_presence_of :name, length: {minimum: 2}
   validates_format_of :email, :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
-  after_create :subscribe
+  after_commit :subscribe
   
   def subscribe
     MailingListSignupJob.perform_later(self)
