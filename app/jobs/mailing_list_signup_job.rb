dee8de2d2b9eef4c37156a5895d24b0082880ad3
Harry Oliver <someonecancode@yahoo.co.uk>
2016-02-16 09:49:36 +0000

    remove rescue block

--- app/jobs/mailing_list_signup_job.rb
+++ app/jobs/mailing_list_signup_job.rb
@@ -9,16 +9,13 @@ class MailingListSignupJob < ActiveJob::Base
     mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
     list_id = Rails.application.secrets.mailchimp_list_id
     
-    begin
     result = mailchimp.lists(list_id).members.create(
       body: {
         email_address: user.email,
         status: 'subscribed',
         merge_fields: {FNAME: user.name}
       })
-      rescue Gibbon::MailChimpError => e
-        flash[:error] = "There was an error #{e.message}"
-      end
+     
       Rails.logger.info("Subscribed #{user.email} to mailchimp") if result
   end
   
